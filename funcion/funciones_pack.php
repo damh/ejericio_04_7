92a0452088dc556108aac7ffecf0f2366277f503
DAMH1 <81368782+damh@users.noreply.github.com>
2021-04-07 09:24:10 -0500

    Add files via upload

---
+++ funcion/funciones_pack.php
@@ -0,0 +1,14 @@
+<?php
+
+function suma ($p, $q,$w)
+{
+    $suma = $p + $q + $w;
+    echo "la suma es = $suma";
+    //echo "<br></br>$pe";
+    //echo "<br></br>$w";
+
+}
+function negativo ($numero)
+{
+    return $numero * -1;
+}
---
+++ funcion/llamado.php
@@ -0,0 +1,4 @@
+<?php
+include "funciones_pack.php";
+$b = negativo(20);
+suma( negativo(65),negativo(20),negativo(20));
