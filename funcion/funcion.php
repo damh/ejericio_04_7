b7d7fb63e9b90fe461fa225b93447117bf484599
DAMH1 <81368782+damh@users.noreply.github.com>
2021-04-07 08:36:57 -0500

    Add files via upload

---
+++ funcion/funcion.php
@@ -0,0 +1,25 @@
+<?php
+
+paso1(1,0);
+function paso1($paso, $q)
+{
+    
+    for ($paso=-10; $paso <= 10; $paso ++)
+  {
+      if ($paso < 0)
+      {
+          echo "<br><b>$paso</b><br>";
+      }
+    else
+    {
+        echo "<br>$paso.</br>";
+    }
+      
+  }
+  
+}
+
+
+
+
+
